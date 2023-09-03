cd32f45d990950ebf94276fb90c12d2d5fdbcb72
Hernan Urban <hernanurban@MacBook-Pro-de-Hernan.local>
2023-09-03 12:11:24 -0300

    Initial commit

---
+++ src/main/java/com/hurbanlab/catalog/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.hurbanlab.catalog.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${spring.application.name:catalog}")
+    private String appName;
+
+    @RequestMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello " + appName, HttpStatus.OK);
+    }
+}
