a886896cf5d39b3582cbf37b7ff16f764027a467
Hernan Urban <hernanurban@MacBook-Pro-de-Hernan.local>
2023-09-05 19:24:16 -0300

    Adding unit tests

--- src/main/java/com/hurbanlab/catalog/service/ProductService.java
+++ src/main/java/com/hurbanlab/catalog/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.hurbanlab.catalog.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -8,14 +9,23 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ProductService {
 
-    @Value("${product.host:http://localhost:9090}")
     private String productHost;
 
-    @Value("${product.get-hello-uri:/hello}")
     private String uri;
 
-    private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate;
 
+    public ProductService(
+            @Value("${product.host:http://localhost:9090}")
+            String productHost,
+            @Value("${product.get-hello-uri:/hello}")
+            String uri,
+            @Autowired RestTemplate restTemplate
+    ){
+        this.restTemplate = restTemplate;
+        this.uri = uri;
+        this.productHost = productHost;
+    }
 
     public String getProductHello() {
         String fooResourceUrl = productHost + uri;
